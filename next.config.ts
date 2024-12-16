fd9d9a86e45ae1c7c068c7be363481cce648ab3b
Viktor Barta <viktor.barta@techzia.hu>
2024-12-16 16:30:52 +0100

    try to fix build erro on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,18 @@
-import type { NextConfig } from 'next';
-const createNextIntlPlugin = require('next-intl/plugin');
+/* eslint-disable @typescript-eslint/no-require-imports */
+import type { NextConfig } from 'next'
+const createNextIntlPlugin = require('next-intl/plugin')
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  env: {
-    WOO_KEY: process.env.WOO_KEY,
-    WOO_SECRET: process.env.WOO_SECRET,
-  },
-  images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'liftyofficial.com' }],
-  },
-};
+    output: 'standalone',
+    env: {
+        WOO_KEY: process.env.WOO_KEY,
+        WOO_SECRET: process.env.WOO_SECRET,
+    },
+    images: {
+        remotePatterns: [{ protocol: 'https', hostname: 'liftyofficial.com' }],
+    },
+}
 
-module.exports = withNextIntl(nextConfig);
+module.exports = withNextIntl(nextConfig)
