1f28381cfc4f555e6ddad48458bd8fb74e899ab9
Viktor Barta <viktor.barta@techzia.hu>
2024-12-17 15:00:09 +0100

    add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,14 +1,56 @@
-export const Footer = () => {
+'use server'
+
+import { getTranslations } from 'next-intl/server'
+import Image from 'next/image'
+import { Separator } from '../ui/separator'
+import { EmailSub } from './email-sub/EmailSub'
+
+export const Footer = async () => {
+    const t = await getTranslations()
+
     return (
         <footer className="bg-secondary p-8">
-            <div className="container mx-autoflex flex flex-row justify-between">
-                <div>col 1</div>
-                <div>col 2</div>
-                <div>col 3</div>
-                <div>col 4</div>
+            <div className="container mx-auto flex flex-row justify-between">
+                <div className="flex flex-col">
+                    <h2 className="text-2xl">{t('Common.informations')}</h2>
+                    <p className="text-sm">{t('Common.shipping')}</p>
+                    <p className="text-sm">{t('Common.refund')}</p>
+                    <p className="text-sm">{t('Common.privacy_policy')}</p>
+                    <p className="text-sm">{t('Common.t_c')}</p>
+                </div>
+                <div className="flex flex-col">
+                    <h2 className="text-2xl">{t('Common.quick_links')}</h2>
+                    <p className="text-sm">{t('Common.shop')}</p>
+                    <p className="text-sm">{t('Common.contact')}</p>
+                    <p className="text-sm">{t('Common.about_us')}</p>
+                    <p className="text-sm">{t('Common.carrier_instuctions')}</p>
+                </div>
+                <div className="flex flex-col">
+                    <Image
+                        src="/images/logo-small.webp"
+                        width={120}
+                        height={0}
+                        alt="lifty-logo"
+                        style={{
+                            objectFit: 'contain',
+                            maxWidth: '100%',
+                            height: 'auto',
+                        }}
+                        className="w-16 md:w-20 lg:w-24"
+                    />
+                    <EmailSub />
+                </div>
             </div>
-            <div className="mt-4">
+            <Separator className="mt-4" />
+            <div className="mt-4 container mx-auto flex flex-col items-center justify-center space-y-4">
                 <p className="text-xs text-slate-500">2024 ©lifty, All rights reserved</p>
+                <Image
+                    src="/images/stripe-cc-payments1.webp"
+                    alt="stripe"
+                    width={300}
+                    height={120}
+                    style={{ maxWidth: '100%', height: 'auto', objectFit: 'contain' }}
+                />
             </div>
         </footer>
     )
--- src/components/herobanner/Herobanner.tsx
+++ src/components/herobanner/Herobanner.tsx
@@ -1,14 +1,27 @@
 'use client'
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
+import { Link } from '@/i18n/routing'
 import { routes } from '@/utils/routes'
 import Autoplay from 'embla-carousel-autoplay'
 import Fade from 'embla-carousel-fade'
 import Image from 'next/image'
-import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
 import hero1 from './media/hero-1.webp'
 import hero2 from './media/hero-2.webp'
+
+const carousels = [
+    {
+        src: hero1.src,
+        title: 'Lifty',
+        desc: 'The toddler hipp carrier',
+    },
+    {
+        src: hero2.src,
+        title: 'Lifty',
+        desc: 'The essential for adventures',
+    },
+]
 
 export const Herobanner = () => {
     const [autoPlayPlugin] = React.useState(() => Autoplay({ delay: 4000, stopOnInteraction: true }))
@@ -22,13 +35,14 @@ export const Herobanner = () => {
             plugins={[fadePlugin, autoPlayPlugin]}
             className="w-full"
         >
-            <CarouselContent className="min-h-[600px]">
-                <CarouselItem>
-                    <CarouselImageWrapper src={hero1.src} heading="Lifty" desc="The toddler hipp carrier" />
-                </CarouselItem>
-                <CarouselItem>
-                    <CarouselImageWrapper src={hero2.src} heading="Lifty" desc="The essential for adventures" />
-                </CarouselItem>
+            <CarouselContent className="min-h-[600px] aspect-[1920/600]">
+                {carousels.map(item => {
+                    return (
+                        <CarouselItem key={item.desc}>
+                            <CarouselImageWrapper src={item.src} heading={item.title} desc={item.desc} />
+                        </CarouselItem>
+                    )
+                })}
             </CarouselContent>
         </Carousel>
     )
@@ -43,7 +57,7 @@ interface CarouselImageWrapperProps {
 const CarouselImageWrapper: React.FC<CarouselImageWrapperProps> = ({ src, heading, desc }) => {
     return (
         <div className="h-full w-dvw relative">
-            <div className="absolute left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] bg-zinc-800/40 z-2 w-[100%] h-[100%] flex flex-col justify-center items-center">
+            <div className="z-10 absolute left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%] bg-zinc-800/40 z-2 w-[100%] h-[100%] flex flex-col justify-center items-center">
                 <h1 className="text-8xl text-white text-left font-bold tracking-wide">{heading}</h1>
                 <h2 className="text-3xl text-white line-clamp-none text-left mt-2">{desc}</h2>
                 <Link href={routes.shop}>
