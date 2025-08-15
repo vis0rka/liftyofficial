3773e299ddf795563a1f5c22ea0c5b43174eebc0
Viktor Barta <barta.viktor.ufs@gmail.com>
2025-08-15 07:46:50 +0200

    add revalidate tag and path api

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,29 @@
+import { revalidatePath, revalidateTag } from 'next/cache'
+import type { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+    const secret = request.nextUrl.searchParams.get('secret')
+
+    if (secret !== 'SECRET') {
+        return Response.json({ message: 'Invalid secret' }, { status: 401 })
+    }
+
+    const tag = request.nextUrl.searchParams.get('tag')
+
+    if (tag) {
+        revalidateTag(tag)
+        return Response.json({ revalidated: true })
+    }
+
+    const path = request.nextUrl.searchParams.get('path')
+
+    if (path) {
+        revalidatePath(path)
+        return Response.json({ revalidated: true })
+    }
+
+    return Response.json({
+        revalidated: false,
+        message: 'Missing path or tag to revalidate',
+    })
+}
